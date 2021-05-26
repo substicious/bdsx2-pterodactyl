893f784e9e0358b276631d1f2f0388ddb2d4cdf9
Substicious <68825499+substicious@users.noreply.github.com>
2021-05-26 18:11:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Make sure we're in the correct dir
-cd /BDSx2
+cd /home/container/BDSx2
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/BDSx2/$ ${MODIFIED_STARTUP}"
+echo ":/home/container/BDSx2/$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
